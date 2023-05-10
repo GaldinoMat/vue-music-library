0b6ee92b45d8a29b061fc6e24edd92972d514141
GaldinoMat <mateusgaldino1000@hotmail.com>
2023-05-10 18:32:23 -0300

    feat: implement CRUD operations in MusicList component

---
+++ src/components/MusicList/components/MusicCompositionItem/MusicCompositionItem.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="border border-gray-200 p-3 mb-4 rounded">
+    <div v-show="!showForm">
+      <h4 class="inline-block text-2xl font-bold">{{ song.modifiedName }}</h4>
+      <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
+        <i class="fa fa-times" @click.prevent="deleteSong"></i>
+      </button>
+      <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
+        <i class="fa fa-pencil-alt" @click.prevent="showForm = !showForm"></i>
+      </button>
+    </div>
+    <div v-show="showForm">
+      <AppAuthFeedbackInfo
+        :actionShowAlert="editShowAlert"
+        :actionAlertVariant="editAlertVariant"
+        :actionAlertMessage="editAlertMessage"
+      />
+      <VeeForm :validationSchema="schema" :initial-values="songData" @submit="editMusic">
+        <div class="mb-3">
+          <label class="inline-block mb-2">Song Title</label>
+          <VeeField
+            type="text"
+            name="SongTitle"
+            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+            placeholder="Enter Song Title"
+          />
+          <ErrorMessage class="text-red-600" name="SongTitle" />
+        </div>
+        <div class="mb-3">
+          <label class="inline-block mb-2">Genre</label>
+          <VeeField
+            type="text"
+            name="SongGenre"
+            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+            placeholder="Enter Genre"
+          />
+          <ErrorMessage class="text-red-600" name="SongGenre" />
+        </div>
+        <button
+          type="submit"
+          class="py-1.5 px-3 rounded text-white bg-green-600"
+          :disabled="editInSubmission"
+        >
+          Submit
+        </button>
+        <button
+          type="button"
+          class="py-1.5 px-3 rounded text-white bg-gray-600"
+          @click.prevent="resetCompositionItem"
+          :disabled="editInSubmission"
+        >
+          Go Back
+        </button>
+      </VeeForm>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppAuthFeedbackInfo from '../../../AuthModal/components/AuthFeedbackInfo/AppAuthFeedbackInfo.vue'
+import { songsCollection, storage } from '@/includes/firebase.js'
+
+export default {
+  name: 'MusicCompositionItem',
+  data() {
+    return {
+      showForm: false,
+      schema: {
+        SongTitle: 'required',
+        SongGenre: 'alphaSpaces'
+      },
+      songData: {
+        SongTitle: this.song.modifiedName,
+        SongGenre: this.song.genre
+      },
+      editInSubmission: false,
+      editShowAlert: false,
+      editAlertVariant: '',
+      editAlertMessage: ''
+    }
+  },
+  props: {
+    song: {
+      type: Object,
+      required: true
+    },
+    index: {
+      type: Number,
+      required: true
+    },
+    updateSong: {
+      type: Function,
+      required: true
+    },
+    removeSong: {
+      type: Function,
+      required: true
+    }
+  },
+  components: { AppAuthFeedbackInfo },
+  methods: {
+    async editMusic(values) {
+      this.editInSubmission = true
+      this.editShowAlert = true
+      this.editAlertVariant = 'bg-blue-500'
+      this.editAlertMessage = 'Please wait! Updating song info.'
+
+      try {
+        await songsCollection.doc(this.song.docId).update({
+          modifiedName: values.SongTitle,
+          genre: values.SongGenre
+        })
+      } catch (error) {
+        this.editInSubmission = false
+        this.editShowAlert = false
+        this.editAlertVariant = 'bg-red-500'
+        this.editAlertMessage = 'Something went wrong! Please try again later.'
+
+        return
+      }
+
+      this.updateSong(this.index, values)
+
+      this.editInSubmission = false
+      this.editAlertVariant = 'bg-green-500'
+      this.editAlertMessage = 'Success!'
+    },
+    async deleteSong() {
+      const storageRef = storage.ref()
+      const songRef = storageRef.child(`songs/${this.song.originalName}`)
+
+      await songRef.delete()
+
+      await songsCollection.doc(this.song.docId).delete()
+
+      this.removeSong(this.index)
+    },
+    resetCompositionItem() {
+      this.showForm = false
+      this.editShowAlert = false
+    }
+  }
+}
+</script>
--- src/views/ManageView.vue
+++ src/views/ManageView.vue
@@ -13,109 +13,59 @@
         -->
         <MusicUpload ref="upload" />
       </div>
-      <div class="col-span-2">
-        <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-          <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-            <span class="card-title">My Songs</span>
-            <i class="fa fa-compact-disc float-right text-green-400 text-2xl"></i>
-          </div>
-          <div class="p-6">
-            <!-- Composition Items -->
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                  <i class="fa fa-times"></i>
-                </button>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-              <div>
-                <form>
-                  <div class="mb-3">
-                    <label class="inline-block mb-2">Song Title</label>
-                    <input
-                      type="text"
-                      class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                      placeholder="Enter Song Title"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label class="inline-block mb-2">Genre</label>
-                    <input
-                      type="text"
-                      class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                      placeholder="Enter Genre"
-                    />
-                  </div>
-                  <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">
-                    Submit
-                  </button>
-                  <button type="button" class="py-1.5 px-3 rounded text-white bg-gray-600">
-                    Go Back
-                  </button>
-                </form>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                  <i class="fa fa-times"></i>
-                </button>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                  <i class="fa fa-times"></i>
-                </button>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                  <i class="fa fa-times"></i>
-                </button>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                  <i class="fa fa-times"></i>
-                </button>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <MusicList>
+        <!-- Composition Items -->
+        <MusicCompositionItem
+          v-for="(song, index) in songs"
+          :key="song.docId"
+          :song="song"
+          :updateSong="updateSong"
+          :index="index"
+          :removeSong="removeSong"
+        />
+      </MusicList>
     </div>
   </section>
 </template>
 
 <script>
-import MusicUpload from '../components/MusicUpload/MusicUpload.vue'
+import MusicUpload from '@/components/MusicUpload/MusicUpload.vue'
+import MusicList from '@/components/MusicList/MusicList.vue'
+import MusicCompositionItem from '@/components/MusicList/components/MusicCompositionItem/MusicCompositionItem.vue'
+import { songsCollection, auth } from '@/includes/firebase.js'
 
 export default {
   name: 'ManageView',
   components: {
-    MusicUpload
+    MusicUpload,
+    MusicList,
+    MusicCompositionItem
+  },
+  data() {
+    return {
+      songs: []
+    }
+  },
+  methods: {
+    updateSong(index, values) {
+      this.songs[index].modifiedName = values.SongTitle
+      this.songs[index].genre = values.SongGenre
+    },
+    removeSong(index) {
+      this.songs.splice(index, 1)
+    }
+  },
+  async created() {
+    const snapshot = await songsCollection.where('uid', '==', auth.currentUser.uid).get()
+
+    snapshot.forEach((document) => {
+      const song = {
+        ...document.data(),
+        docId: document.id
+      }
+
+      this.songs.push(song)
+    })
   }
   /**
     This is a Navigation guard for components. Contrary to the ohter two possibilities,
