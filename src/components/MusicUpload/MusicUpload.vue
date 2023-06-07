4730ca4e6650f759a81a690c5819f07fa53334cf
GaldinoMat <mateusgaldino1000@hotmail.com>
2023-06-06 23:37:09 -0300

    feat: convert ManageView and components to composition API

--- src/components/MusicUpload/MusicUpload.vue
+++ src/components/MusicUpload/MusicUpload.vue
@@ -6,7 +6,7 @@
     </div>
     <div class="p-6">
       <DragComponent
-        @changeDragState="changeDragState"
+        @change-drag-state="changeDragState"
         :isDraggedOver="isDraggedOver"
         :uploadFile="uploadFile"
       />
@@ -17,116 +17,105 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { onBeforeUnmount, reactive, ref } from 'vue'
 import { storage, auth, songsCollection } from '@/includes/firebase.js'
 import ProgressBar from './components/ProgressBar/ProgressBar.vue'
 import DragComponent from './components/DragComponent/DragComponent.vue'
 
-export default {
-  name: 'MusicUpload',
-  data() {
-    return {
-      isDraggedOver: false,
-      uploads: []
-    }
-  },
-  props: {
-    addSong: {
-      type: Function,
-      required: true
-    }
-  },
-  components: { ProgressBar, DragComponent },
-  methods: {
-    changeDragState(newsState) {
-      this.isDraggedOver = newsState
-    },
-    uploadFile(fileEvent) {
-      this.isDraggedOver = false
+let isDraggedOver = ref(false)
+const uploads = reactive([])
 
-      const files = fileEvent.dataTransfer
-        ? [...fileEvent.dataTransfer.files]
-        : [...fileEvent.target.files]
-
-      files.forEach((file) => {
-        if (file.type !== 'audio/mpeg') {
-          return
-        }
-
-        if (!navigator.onLine) {
-          this.uploads.push({
-            task: {},
-            currentProgress: 100,
-            name: file.name,
-            variant: 'bg-red-400',
-            icon: 'fas fa-times',
-            textClass: 'text-red-400'
-          })
-          return
-        }
-
-        const reference = storage.ref()
-        const songsReference = reference.child(`songs/${file.name}`)
-
-        const task = songsReference.put(file)
-
-        const uploadIndex =
-          this.uploads.push({
-            task,
-            currentProgress: 0,
-            name: file.name,
-            variant: 'bg-blue-400',
-            icon: 'fas fa-spinner fa-spin',
-            textClass: ''
-          }) - 1
-
-        task.on(
-          'state_changed',
-          (snapshot) => {
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-            this.uploads[uploadIndex].currentProgress = progress
-          },
-          () => {
-            this.uploads[uploadIndex].variant = 'bg-red-400'
-            this.uploads[uploadIndex].icon = 'fas fa-times'
-            this.uploads[uploadIndex].textClass = 'text-red-400'
-          },
-          async () => {
-            const song = {
-              uid: auth.currentUser.uid,
-              displayName: auth.currentUser.displayName,
-              originalName: task.snapshot.ref.name,
-              modifiedName: task.snapshot.ref.name,
-              genre: '',
-              commentCount: 0
-            }
-
-            song.url = await task.snapshot.ref.getDownloadURL()
-
-            const songRef = await songsCollection.add(song)
-
-            const songSnapshot = await songRef.get()
-
-            this.addSong(songSnapshot)
-
-            this.uploads[uploadIndex].variant = 'bg-green-400'
-            this.uploads[uploadIndex].icon = 'fas fa-check'
-            this.uploads[uploadIndex].textClass = 'text-green-400'
-          }
-        )
-      })
-    },
-    cancelUploads() {
-      this.uploads.forEach((upload) => {
-        upload.task.cancel()
-      })
-    }
-  },
-  // Lifecycle methods used to safeguard for upload cancel
-  beforeUnmount() {
-    this.uploads.forEach((upload) => {
-      upload.task.cancel()
-    })
+const props = defineProps({
+  addSong: {
+    type: Function,
+    required: true
   }
+})
+
+const changeDragState = (newsState) => {
+  isDraggedOver.value = newsState
 }
+const uploadFile = (fileEvent) => {
+  isDraggedOver.value = false
+
+  const files = fileEvent.dataTransfer
+    ? [...fileEvent.dataTransfer.files]
+    : [...fileEvent.target.files]
+
+  files.forEach((file) => {
+    if (file.type !== 'audio/mpeg') {
+      return
+    }
+
+    if (!navigator.onLine) {
+      uploads.push({
+        task: {},
+        currentProgress: 100,
+        name: file.name,
+        variant: 'bg-red-400',
+        icon: 'fas fa-times',
+        textClass: 'text-red-400'
+      })
+      return
+    }
+
+    const reference = storage.ref()
+    const songsReference = reference.child(`songs/${file.name}`)
+
+    const task = songsReference.put(file)
+
+    const uploadIndex =
+      uploads.push({
+        task,
+        currentProgress: 0,
+        name: file.name,
+        variant: 'bg-blue-400',
+        icon: 'fas fa-spinner fa-spin',
+        textClass: ''
+      }) - 1
+
+    task.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        uploads[uploadIndex].currentProgress = progress
+      },
+      () => {
+        uploads[uploadIndex].variant = 'bg-red-400'
+        uploads[uploadIndex].icon = 'fas fa-times'
+        uploads[uploadIndex].textClass = 'text-red-400'
+      },
+      async () => {
+        const song = {
+          uid: auth.currentUser.uid,
+          displayName: auth.currentUser.displayName,
+          originalName: task.snapshot.ref.name,
+          modifiedName: task.snapshot.ref.name,
+          genre: '',
+          commentCount: 0
+        }
+
+        song.url = await task.snapshot.ref.getDownloadURL()
+
+        const songRef = await songsCollection.add(song)
+
+        const songSnapshot = await songRef.get()
+
+        props.addSong(songSnapshot)
+
+        uploads[uploadIndex].variant = 'bg-green-400'
+        uploads[uploadIndex].icon = 'fas fa-check'
+        uploads[uploadIndex].textClass = 'text-green-400'
+      }
+    )
+  })
+}
+const cancelUploads = () => {
+  uploads.forEach((upload) => {
+    upload.task.cancel()
+  })
+}
+
+onBeforeUnmount(() => cancelUploads())
 </script>
